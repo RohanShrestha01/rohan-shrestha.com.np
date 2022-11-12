5d80a6b34c1e9ab8b3b37187afc9d4b465c83537
Rohan Shrestha <shrestharohan039@gmail.com>
2022-11-12 22:55:07 +0545

    scroll event throttled and debounced to prevent scroll jank

--- src/scripts/header.ts
+++ src/scripts/header.ts
@@ -1,13 +1,16 @@
 // To hide header when user scrolls down and show when the user scrolls up
 const headerEl = document.querySelector('header')!;
 const headerBottom = headerEl.offsetTop + headerEl.offsetHeight;
+
+let timeoutScroll: NodeJS.Timeout; // holder for timeout id
+let scrollThrottle = false;
 let prevScrollPos = window.scrollY;
 
 const scrollHandler = () => {
   const currentScrollPos = window.scrollY;
   if (prevScrollPos > currentScrollPos || currentScrollPos < headerBottom)
-    headerEl.style.top = '0';
-  else headerEl.style.top = `-${headerBottom}px`;
+    headerEl.style.transform = 'translateY(0)';
+  else headerEl.style.transform = `translateY(-${headerBottom}px)`;
 
   // To change styling of header when it is displayed on scroll
   if (currentScrollPos > headerBottom) headerEl.classList.add('scroll-header');
@@ -16,6 +19,20 @@ const scrollHandler = () => {
   prevScrollPos = currentScrollPos;
 };
 
-window.addEventListener('scroll', scrollHandler);
+window.addEventListener('scroll', () => {
+  // Only run if not throttled
+  if (!scrollThrottle) {
+    scrollHandler();
+    scrollThrottle = true;
+    // Set a timeout to un-throttle
+    setTimeout(() => {
+      scrollThrottle = false;
+    }, 400);
+  } else {
+    // debouncing, only when throttled
+    clearTimeout(timeoutScroll);
+    timeoutScroll = setTimeout(scrollHandler, 50);
+  }
+});
 
 export {};
