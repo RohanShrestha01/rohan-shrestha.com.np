3dfbca282119b49d5bf0994f79be9be925f86b5b
Rohan Shrestha <shrestharohan039@gmail.com>
2022-11-12 10:59:15 +0545

    close side drawer/menu when clicked on nav link

--- src/scripts/hamburgerMenu.ts
+++ src/scripts/hamburgerMenu.ts
@@ -2,16 +2,29 @@
 const hamburgerBtnEl = document.getElementById('hamburger-btn')!;
 const navEl = document.getElementById('nav')!;
 const backdropEl = document.getElementById('backdrop')!;
+const navLinkElements = document.getElementsByClassName('nav__link');
 
 const drawerFunctionalityHandler = (): void => {
   hamburgerBtnEl.classList.toggle('active');
   navEl.classList.toggle('visible');
   backdropEl.classList.toggle('visible');
+  document.body.classList.toggle('no-scroll');
 };
 
 hamburgerBtnEl.addEventListener('click', drawerFunctionalityHandler);
 
+const closeDrawer = (): void => {
+  hamburgerBtnEl.classList.remove('active');
+  navEl.classList.remove('visible');
+  backdropEl.classList.remove('visible');
+  document.body.classList.remove('no-scroll');
+};
+
 // Close Drawer when clicked
-backdropEl.addEventListener('click', drawerFunctionalityHandler);
+backdropEl.addEventListener('click', closeDrawer);
+
+// Close Drawer when clicked on any nav links
+for (const navLink of navLinkElements)
+  navLink.addEventListener('click', closeDrawer);
 
 export {};
